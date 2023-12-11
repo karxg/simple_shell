be83db1ef9808bcb1f2c3b42a43bb6ec68066dc4
abotza <botza.amine@gmail.com>
2023-12-11 19:00:17 +0100

    adding checks

--- checks.c
+++ checks.c
@@ -33,4 +33,65 @@ int _checkdelim(char c, char *delim)
 			return (1);
 	return (0);
 }
+/**
+* _checkcmd - determines if a file is an executable command
+* @info: pointer to info struct containing information
+* @p: path to the file to be checked
+* Return: 1 if the file is an executable command, 0 otherwise.
+*
+* Description: This function checks whether the file specified by
+* the path @p is an executable command. It uses the provided
+* arginfo struct @info for context.
+*/
 
+int _checkcmd(arginfo *info, char *p)
+{
+    struct stat st;
+
+    (void)info;
+    if (!p || stat(p, &st) != 0)
+        return (0);
+
+    return (S_ISREG(st.st_mode));
+}
+
+/**
+* _checkprefix - check if @prefix is a prefix of the @scr
+* @src: string to search within
+* @prefix: prefix to find
+* Return: pointer to position after matched prefix in @src
+* or NULL if not found.
+*
+* Description: Compare characters in @src and @prefix until the end of
+* @prefix or a mismatch is found. Return position after the
+* matched prefix in @src, or NULL if not found.
+*/
+
+char *_checkprefix(const char *src, const char *prefix)
+{
+    while (*prefix != '\0' && *src != '\0' && *prefix == *src)
+    {
+        prefix++;
+        src++;
+    }
+
+    return ((*prefix == '\0') ? (char *)src : NULL);
+}
+
+/**
+ * _checksigint - handle the Ctrl C case
+ * @n:  signal number
+ * Return: void
+ *
+ * Description: This function is a signal handler for
+ * the Ctrl+C signal (SIGINT).It writes a newline and the shell prompt to
+ * the standard output.
+ */
+
+void _checksigint(int n)
+{
+    if (n == SIGINT)
+    {
+        write(STDOUT_FILENO, "\n$ ", 3);
+    }
+}
